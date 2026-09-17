8698e6944624cfcbc1d1955a14a71385f810a4f1
Sera <gseraria@gmail.com>
2026-09-17 14:52:15 +0200

    refactoring done

---
+++ blume.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "blume";
+
+export default defineConfig({
+  title: "NFDI4Memory Dokumentation",
+  description: "Dokumentation der Problem-Story-Karten für WordPress.",
+});
